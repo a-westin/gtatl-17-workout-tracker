64c50f3f37df1d43fe1e5a457e3ff3890b899d83
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-14 22:45:30 -0500

    updated virtual for loop

--- models/workout.js
+++ models/workout.js
@@ -2,53 +2,58 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: false,
-        required: "Type is required.",
-      },
-      name: {
-        type: String,
-        trim: false,
-        required: "Name is required.",
-      },
-      duration: {
-        type: Number,
-        required: false,
-      },
-      weight: {
-        type: Number,
-        required: false,
-      },
-      distance: {
-        type: Number,
-        required: false,
-      },
-      reps: {
-        type: Number,
-        required: false,
-      },
-      sets: {
-        type: Number,
-        required: false,
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-}, { toJSON: { virtuals: true } }
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: false,
+          required: "Type is required.",
+        },
+        name: {
+          type: String,
+          trim: false,
+          required: "Name is required.",
+        },
+        duration: {
+          type: Number,
+          required: false,
+        },
+        weight: {
+          type: Number,
+          required: false,
+        },
+        distance: {
+          type: Number,
+          required: false,
+        },
+        reps: {
+          type: Number,
+          required: false,
+        },
+        sets: {
+          type: Number,
+          required: false,
+        },
+      },
+    ],
+  },
+  { toJSON: { virtuals: true } }
 );
-// virtual for totalDuration amount
+
+// Virtual for totalDuration amount
 WorkoutSchema.virtual("totalDuration").get(function () {
-  return this.exercises.reduce((totalDuration, exercise) => {
-    return totalDuration + exercise.duration;
-  }, 0);
-})
+  let totalDuration = 0;
+  for (let i = 0; i < this.exercises.length; i++) {
+    totalDuration = totalDuration + this.exercises[i].duration;
+  }
+  return totalDuration;
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
