66ceb24a9fdd98d622794b88edd2f1bb93cd7b82
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-14 22:26:19 -0500

    added html routes to workoutController

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const path = require("path");
+
+// Getting HTML routes
+router.get('/exercise', function (req, res) {
+
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+
+});
+
+router.get('/stats', function (req, res) {
+
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+
+});
+
+router.get("/", function (req, res) {
+
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
+
 router.get("/api/workouts", function (req, res) {
   db.Workout.find({})
     .then((workouts) => {
